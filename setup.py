dd12e786590a5935b47afb95b70559153238cda4
Fredric Vollmer <fredricvollmer@Fredrics-iMac.local>
2017-04-16 23:29:30 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='PythonCore',
+    version='0.1',
+    packages=['pythoncore'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
