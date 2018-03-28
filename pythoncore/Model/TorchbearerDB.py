da618817b4ce6825790eebcc1c2064d7cad0ec72
Fredric Vollmer <fredricvollmer@Fredrics-iMac.local>
2018-03-27 21:25:35 -0600

    Expose engine

--- pythoncore/Model/TorchbearerDB.py
+++ pythoncore/Model/TorchbearerDB.py
@@ -1,12 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from ..Vault import get_key
-from Hit import Hit
-
 
 __connection_string = get_key('torchbearerdb-connection-string')
 
-__engine = create_engine(__connection_string, echo=False, pool_recycle=3600)
+Engine = create_engine(__connection_string, echo=False, pool_recycle=3600)
 
 # Create session maker, to be used throughout application
-Session = sessionmaker(bind=__engine)
+Session = sessionmaker(bind=Engine)
